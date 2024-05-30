c0a7c202fa124bc8c4350ea3ffff2af46a1cb03f
Mohd Kaif <98801504+KaifAhmad1@users.noreply.github.com>
2024-05-30 16:11:32 +0530

    Update model.py

--- model.py
+++ model.py
@@ -43,7 +43,7 @@ class HFModel(LM):
 
 def initialize_model():
     bnb_config = BitsAndBytesConfig(
-        load_in 4bit=True,
+        load_in_4bit=True,
         bnb_4bit_quant_type='nf4',
         bnb_4bit_use_double_quant=True,
         bnb_4bit_compute_dtype=torch.bfloat16
